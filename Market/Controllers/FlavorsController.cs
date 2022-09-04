df2fbe769c23cc8d02bcd434202bf89813bdcd14
Elliot McGonigal <elliot@elliotmcgonigal.com>
2022-09-04 14:46:35 -0700

    give controllers same functionality

--- Market/Controllers/FlavorsController.cs
+++ Market/Controllers/FlavorsController.cs
@@ -1,35 +1,54 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using Market.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace Market.Controllers
 {
+  [Authorize]
   public class FlavorsController : Controller
   {
     private readonly MarketContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
 
-    public FlavorsController(MarketContext db)
+    public FlavorsController(UserManager<ApplicationUser> userManager, MarketContext db)
     {
+      _userManager = userManager;
       _db = db;
     }
 
-    public ActionResult Index()
+    public async Task<ActionResult> Index()
     {
-      List<Flavor> model = _db.Flavors.ToList();
-      return View(model);
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      var userFlavors = _db.Flavors.Where(entry => entry.User.Id == currentUser.Id).ToList();
+      return View(userFlavors);
     }
 
     public ActionResult Create()
     {
+      ViewBag.SweetId = new SelectList(_db.Sweets, "SweetId", "Name");
       return View();
     }
 
     [HttpPost]
-    public ActionResult Create(Flavor flavor)
+    public async Task<ActionResult> Create(Flavor flavor, int SweetId)
     {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      flavor.User = currentUser;
       _db.Flavors.Add(flavor);
+      _db.SaveChanges();
+      if (SweetId != 0)
+      {
+          _db.FlavorSweet.Add(new FlavorSweet() { FlavorId = flavor.FlavorId, SweetId = SweetId });
+      }
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
@@ -45,13 +64,36 @@ namespace Market.Controllers
     public ActionResult Edit(int id)
     {
       var thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+      ViewBag.SweetId = new SelectList(_db.Sweets, "SweetId", "Name");
       return View(thisFlavor);
     }
 
     [HttpPost]
-    public ActionResult Edit(Flavor flavor)
+    public ActionResult Edit(Flavor flavor, int SweetId)
     {
+      if (SweetId != 0)
+      {
+        _db.FlavorSweet.Add(new FlavorSweet() { FlavorId = flavor.FlavorId, SweetId = SweetId });
+      }
       _db.Entry(flavor).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult AddSweet(int id)
+    {
+      var thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+      ViewBag.SweetId = new SelectList(_db.Sweets, "SweetId", "Name");
+      return View(thisFlavor);
+    }
+
+    [HttpPost]
+    public ActionResult AddSweet(Flavor flavor, int SweetId)
+    {
+      if (SweetId != 0)
+      {
+      _db.FlavorSweet.Add(new FlavorSweet() { FlavorId = flavor.FlavorId, SweetId = SweetId });
+      }
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
@@ -67,6 +109,15 @@ namespace Market.Controllers
     {
       var thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
       _db.Flavors.Remove(thisFlavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public ActionResult DeleteSweet(int joinId)
+    {
+      var joinEntry = _db.FlavorSweet.FirstOrDefault(entry => entry.FlavorSweetId == joinId);
+      _db.FlavorSweet.Remove(joinEntry);
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
